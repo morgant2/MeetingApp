6276ddb1061e61a86615ab79682f46cc84ebffb7
Tommy Morgan <morgant2@nku.edu>
2017-10-02 14:52:14 -0400

    Adding ProfileActivity Adding buttons to MainActivity and set up event to ProfileActivity

--- app/src/main/java/com/meetingapp/MainActivity.java
+++ app/src/main/java/com/meetingapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.meetingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test
+
+        setupButtonEvents();
     }
+
+    private void setupButtonEvents()
+    {
+        Button profile = (Button) findViewById(R.id.Profile);
+        Button contacts = (Button) findViewById(R.id.Contacts);
+        Button createNewMeeting = (Button) findViewById(R.id.CreateMeeting);
+        Button meetingList = (Button) findViewById(R.id.MeetingList);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent profileIntent = new Intent(MainActivity.this, ProfileActivity.class);
+                MainActivity.this.startActivity(profileIntent);
+            }
+        });
+    }
+
 }
