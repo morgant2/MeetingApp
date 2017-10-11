e7fe531346d248a85a8f6096ad585b9f1a6103fe
Tommy Morgan <morgant2@nku.edu>
2017-10-11 09:52:57 -0400

    Added placeholder items for demo

--- app/src/main/java/com/meetingapp/Activities/CreateMeetingActivity.java
+++ app/src/main/java/com/meetingapp/Activities/CreateMeetingActivity.java
@@ -2,6 +2,10 @@ package com.meetingapp.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.meetingapp.R;
 
@@ -11,6 +15,18 @@ public class CreateMeetingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meeting);
+
+        Button btnAddAttendee = (Button) findViewById(R.id.btnAddAttendee);
+        btnAddAttendee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListView lvAttendees = (ListView) findViewById(R.id.lvAttendees);
+                Spinner spinAddAttendee = (Spinner) findViewById(R.id.spinAddAttendee);
+
+                
+                spinAddAttendee.getSelectedItem().toString();
+            }
+        });
     }
 
 
