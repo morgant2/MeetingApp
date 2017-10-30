3355b511046fd588aa7a6ae70d87181ae0798219
morgant2 <morgant2@nku.edu>
2017-10-30 18:02:05 -0400

    Started setting up Time and Location Fragments - Going to talk to Michael about setting this up correctly

--- app/src/main/java/com/meetingapp/Activities/CreateMeetingActivity.java
+++ app/src/main/java/com/meetingapp/Activities/CreateMeetingActivity.java
@@ -1,12 +1,15 @@
 package com.meetingapp.Activities;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import com.meetingapp.Fragments.TimeFragment;
 import com.meetingapp.R;
 
 public class CreateMeetingActivity extends AppCompatActivity {
@@ -17,6 +20,23 @@ public class CreateMeetingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_meeting);
 
         Button btnAddAttendee = (Button) findViewById(R.id.btnAddAttendee);
+        TimeFragment startTimeFragment = (TimeFragment) getSupportFragmentManager().findFragmentById(R.id.startTime);
+        TimeFragment endTimeFragment = (TimeFragment) getSupportFragmentManager().findFragmentById(R.id.endTime);
+
+        FragmentTransaction startTimeTransaction = getFragmentManager().beginTransaction();
+
+        if(startTimeFragment != null)
+        {
+
+        }
+
+        if(endTimeFragment != null)
+        {
+
+        }
+
+
+
         btnAddAttendee.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
