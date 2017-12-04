d8e7c2bffb0944b63b9df5f6ea4d8be44ef3e842
Jovid <Jovidino@yahoo.com>
2017-12-04 02:25:52 -0500

    Jovid Commit - Scheduled meetings pop up

--- app/src/main/java/com/meetingapp/activities/ScheduledMeetingsActivity.java
+++ app/src/main/java/com/meetingapp/activities/ScheduledMeetingsActivity.java
@@ -1,11 +1,30 @@
 package com.meetingapp.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.meetingapp.R;
+import com.meetingapp.businessObjects.Meeting;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ScheduledMeetingsActivity extends BaseActivity {
+    ArrayList<Meeting> meetingsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +35,37 @@ public class ScheduledMeetingsActivity extends BaseActivity {
          Adding our layout to parent class frame layout.
          */
         getLayoutInflater().inflate(R.layout.activity_scheduled_meetings, mFrameLayout);
+
+        try {
+            setMeetings();
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        ListView list = (ListView) findViewById(R.id.listViewMeetings);
+        list.setAdapter(new ArrayAdapter<Meeting>(this, android.R.layout.simple_list_item_1, meetingsList));
+
+    }
+
+    public void setMeetings() throws JSONException {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.enableComplexMapKeySerialization().create();
+
+        String meetingsJson = getSharedPreferences(getString(R.string.meetings_key), Context.MODE_PRIVATE).getString(getString(R.string.meetings_key), "{'meetings':[]");
+        if (meetingsJson == "") meetingsJson = "{'meetings':[]}";
+        JSONObject Jo = new JSONObject(meetingsJson);
+        JSONArray jsonArray = Jo.getJSONArray("meetings");
+        Type type = new TypeToken<Meeting>(){}.getType();
+
+        for(int i = 0; i < jsonArray.length(); i++)
+        {
+         meetingsList.add((Meeting) gson.fromJson(jsonArray.get(i).toString(),type));
+        }
+
+        if(meetingsList == null || !(meetingsList.size() > 0))
+        {
+            throw new JSONException("No Meetings");
+        }
     }
 }
