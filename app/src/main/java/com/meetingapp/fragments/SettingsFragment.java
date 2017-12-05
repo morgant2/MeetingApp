8080f0344bcaabf699464a15193e0cb32f18fcd4
heilgraeham <31523946+heilgraeham@users.noreply.github.com>
2017-12-05 00:26:25 -0500

    back navigation when drawer is open, auto apply theme update in settings

--- app/src/main/java/com/meetingapp/fragments/SettingsFragment.java
+++ app/src/main/java/com/meetingapp/fragments/SettingsFragment.java
@@ -1,31 +1,67 @@
 package com.meetingapp.fragments;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import com.meetingapp.R;
 
+public class SettingsFragment extends PreferenceFragment
+        implements OnSharedPreferenceChangeListener {
 
-public class SettingsFragment extends PreferenceFragment {
-    private SharedPreferences.OnSharedPreferenceChangeListener mListener;
+    private SharedPreferences prefs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
         setHasOptionsMenu(true);
-        mListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-            @Override
-            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-                if (!key.equals("pref_dark_theme")) {
-                    return;
-                }
-//                Intent intent = new Intent(getActivity(), InformationActivity.class);
-//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//                startActivity(intent);
-            }
-        };
+
+        //Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        prefs.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        prefs.unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("pref_dark_theme")){
+            getActivity().recreate();
+        }
     }
 }
+
+//
+//public class SettingsFragment extends PreferenceFragment {
+//    private SharedPreferences.OnSharedPreferenceChangeListener mListener;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        addPreferencesFromResource(R.xml.preferences);
+//        setHasOptionsMenu(true);
+//        mListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+//            @Override
+//            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+//                if (!key.equals("pref_dark_theme")) {
+//                    return;
+//                }
+////                Intent intent = new Intent(getActivity(), InformationActivity.class);
+////                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+////                startActivity(intent);
+//            }
+//        };
+//    }
+//}
