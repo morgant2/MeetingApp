2d392b7f7c9325c684e0b7417a7f8dfe7e5ab907
heilgraeham <31523946+heilgraeham@users.noreply.github.com>
2017-12-06 17:16:57 -0500

    on contact click opens dialog to delete

--- app/src/main/java/com/meetingapp/activities/ContactsActivity.java
+++ app/src/main/java/com/meetingapp/activities/ContactsActivity.java
@@ -1,12 +1,18 @@
 package com.meetingapp.activities;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -52,11 +58,33 @@ public class ContactsActivity extends BaseActivity {
         });
 
         // get an adapter for the contact list
-        ArrayAdapter contactsAdapter = new ContactAdaptor(this, contactList);
+        final ArrayAdapter contactsAdapter = new ContactAdaptor(this, contactList);
         // get the listview from the layout
         ListView contactListView = (ListView) findViewById(R.id.contact_list_view);
         //Set the adapter to adapt the listview
         contactListView.setAdapter(contactsAdapter);
+
+        contactListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                AlertDialog.Builder adb=new AlertDialog.Builder(ContactsActivity.this);
+                adb.setTitle("Delete?");
+                adb.setMessage("Are you sure you want to delete " + contactList.get(position).getLastFirstName());
+                final int positionToRemove = position;
+                adb.setNegativeButton("Cancel", null);
+                adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        contactList.remove(positionToRemove);
+                        try {
+                            delete();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Unable to delete contact.", Toast.LENGTH_LONG).show();
+                        }
+                        contactsAdapter.notifyDataSetChanged();
+                    }});
+                adb.show();
+            }
+        });
     }
 
     @Override
@@ -88,7 +116,7 @@ public class ContactsActivity extends BaseActivity {
         Gson gson = builder.enableComplexMapKeySerialization().create();
 
         String contactsJson = getSharedPreferences(getString(R.string.contacts_key), Context.MODE_PRIVATE).getString(getString(R.string.contacts_key), "{'contacts':[]}");
-        if (contactsJson == "") contactsJson = "{'contacts':[]}";
+        if (contactsJson.equals("")) contactsJson = "{'contacts':[]}";
         JSONObject jsonObject = new JSONObject(contactsJson);
         JSONArray jsonArray = jsonObject.getJSONArray("contacts");
         Type type = new TypeToken<Contact>(){}.getType();
@@ -102,6 +130,29 @@ public class ContactsActivity extends BaseActivity {
         {
             throw new JSONException("No contacts");
         }
-
     }
+
+    public void delete() throws JSONException {
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.contacts_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        JSONObject jsonObject = new JSONObject("{'contacts':[]}");
+        JSONArray jsonArray = jsonObject.getJSONArray("contacts");
+        JSONObject newContactJSONObject = new JSONObject();
+
+
+        for(int i = 0; i < contactList.size(); i++)
+        {
+            Gson contactGson = new Gson();
+            String jsonString = contactGson.toJson(contactList.get(i), Contact.class);
+
+            jsonArray.put(jsonString);
+        }
+
+        newContactJSONObject.put("contacts", jsonArray);
+
+        editor.putString(getString(R.string.contacts_key), newContactJSONObject.toString());
+        editor.apply();
+    }
+
 }
